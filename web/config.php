3dce7b01541c4d110a423e1ef83897b01a75193c
doudoubinga <42226718+doudoubinga@users.noreply.github.com>
2018-09-25 21:20:11 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
+define('APIKEY', 'AIzaSyAurg5k-lp9qQxA_IpC5x48YZ3-cslEQ5Q');
 define('GJ_CODE', 'JP');
 define('SITE_NAME', '大爷常来玩儿啊');
 define('TITLENAME', 'DouDouTube');
